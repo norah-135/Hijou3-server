dc8c7cebd39605f9e18ab319d8364d6d48fa6f1e
norah-135 <norh13335@gmail.com>
2025-09-29 23:29:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def internal_error(error):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
